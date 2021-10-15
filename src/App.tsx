3d0545ba9c5d79c75e32bba388db8acc23056a81
Lucas Souza <lucasvsouza28@gmail.com>
2021-10-14 23:57:44 -0300

    fix: routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { Header, Card } from './components'
+import { BrowserRouter, Route } from 'react-router-dom';
+import { Header } from './components'
 import { HomePage } from './pages/Home';
 import { CountryPage } from './pages/Country';
 
 function App() {
   return (
     <>
-        
         <BrowserRouter>
           <Header />
-        {/* <Switch> */}
           <Route path='/' exact component={HomePage} />
-          <Route path='/:code' exact component={CountryPage} />        
-        {/* </Switch> */}
-      </BrowserRouter>
+          <Route path='/:code' component={CountryPage} />
+        </BrowserRouter>
     </>
   );
 }
